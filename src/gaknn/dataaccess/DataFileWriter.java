8dfe50db9ea4d2722c8b07e607cf45600a341979
thimal <tkempitiya@gmail.com>
2013-08-21 23:55:56 +0530

    abstract filewriter

--- src/gaknn/dataaccess/DataFileWriter.java
+++ src/gaknn/dataaccess/DataFileWriter.java
@@ -1,5 +1,19 @@
 package gaknn.dataaccess;
+import gaknn.core.Instances;
+import gaknn.core.Instance;
+import gaknn.core.FastVector;
+import gaknn.core.Attribute;
+import java.io.IOException;
 
 public class DataFileWriter {
+	 /** the actual data */
+    protected Instances m_Data;
+    
+    protected String m_filePath;
+    
+    public String m_RelationName;
+    
+    public FastVector m_Attributes;
+
 
 }
