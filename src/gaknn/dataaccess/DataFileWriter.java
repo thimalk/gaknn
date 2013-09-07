c67ca3acf83e8c1e246c56e5f8f5a51f49ff0f96
thimal <tkempitiya@gmail.com>
2013-09-07 11:58:36 +0530

    Datafilewriter bug 1

--- src/gaknn/dataaccess/DataFileWriter.java
+++ src/gaknn/dataaccess/DataFileWriter.java
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class DataFileWriter {
 
@@ -73,31 +74,47 @@ public class DataFileWriter {
 		
     }
   public void writeARFFOuput(String filename){
-	  File file = new File(filename+".csv");
+	  File file = new File(filename+".arff");
  	 try{
 		file.createNewFile();
 
 		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		PrintWriter pw=new PrintWriter(fw);
 		BufferedWriter bw = new BufferedWriter(fw);
 		
-		bw.write(ARFF_RELATION+" "+filename+" output");
-		bw.newLine();
-		bw.write(ARFF_ATTRIBUTE+" "+m_className+" {");
+		//bw.write(ARFF_RELATION+" "+filename+" output\n");
+		pw.println(ARFF_RELATION+" "+filename+" output");
+		//bw.newLine();
+		//bw.flush();
+		pw.flush();
+		//bw.write(ARFF_ATTRIBUTE+" "+m_className+" {");
+		pw.print(ARFF_ATTRIBUTE+" "+m_className+" {");
 		for(int i=0;i<m_classArray.length-1;i++){
-			bw.write(m_classArray[i]+",");
+			//bw.write(m_classArray[i]+",");
+			pw.print(m_classArray[i]+",");
 		}
-		bw.write(m_classArray[m_classArray.length-1]+"}");
-		bw.newLine();
-		bw.write(ARFF_ATTRIBUTE+" "+PERCENTAGE+" "+ARFF_ATTRIBUTE_NUMERIC);
-		bw.newLine();
-		bw.write(ARFF_DATA);
-		bw.newLine();
+		//bw.write(m_classArray[m_classArray.length-1]+"}\n");
+		pw.println(m_classArray[m_classArray.length-1]+"}");
+		//bw.newLine();
+		bw.flush();
+		//bw.write(ARFF_ATTRIBUTE+" "+PERCENTAGE+" "+ARFF_ATTRIBUTE_NUMERIC+"\n");
+		pw.println(ARFF_ATTRIBUTE+" "+PERCENTAGE+" "+ARFF_ATTRIBUTE_NUMERIC);
+		//bw.newLine();
+		bw.flush();
+		//bw.write(ARFF_DATA+"\n");
+		pw.println(ARFF_DATA);
+		//bw.newLine();
+		//bw.flush();
 		
 			for(int i=0;i<m_preictions.length;i++){
-				bw.write(m_classArray[m_preictions[i].Index()]+","+m_preictions[i].Value());
-				bw.newLine();
+				//bw.write(m_classArray[m_preictions[i].Index()]+","+m_preictions[i].Value()+"\n");
+				pw.println(m_classArray[m_preictions[i].Index()]+","+m_preictions[i].Value());
+				//bw.newLine();
+				bw.flush();
 			}
 			bw.close();
+			pw.flush();
+			pw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
